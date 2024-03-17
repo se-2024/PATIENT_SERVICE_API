7f9ebb75112e59c7e6fdfb96eef11635613e6a2e
Fritz G. Batroni <fritz.batroni@celeritycommerce.com>
2024-03-17 11:14:43 -0400

    renamed crud to patient_crud for maintainability

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from typing import List
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
-from lib import crud
+from lib import patient_crud
 from lib import response_models
 from lib.database_connection import SessionLocal
 
@@ -27,5 +27,5 @@ async def root():
 
 @app.get("/patients/", response_model=List[response_models.Patient])
 def get_patients(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    patients = crud.get_patients(db, skip=skip, limit=limit)
+    patients = patient_crud.get_patients(db, skip=skip, limit=limit)
     return patients
