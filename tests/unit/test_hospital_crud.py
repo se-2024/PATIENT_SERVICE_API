13cce514bb230b232e0a378ded10ea5156a2a9cb
Kafomar1 <omarkafando@yaho.com>
2024-03-25 00:28:20 -0500

    fixe hospital crud

--- tests/unit/test_hospital_crud.py
+++ tests/unit/test_hospital_crud.py
@@ -1,12 +1,14 @@
 import sys, os
+
+from lib.db_models import Hospital
 sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/.."))
 
 from lib import hospital_crud
 
 def test_mocked_session_get_all_hospitals(mocked_session: hospital_crud.Session):
-    patient_data = hospital_crud.get_patients(mocked_session, 0, 100)
-    assert len(patient_data) > 0
+    hospital_data = hospital_crud.get_hospitals(mocked_session, 0, 100)
+    assert len(hospital_data) >= 0
 
-def test_mocked_session_get_patient_by_id(mocked_session: hospital_crud.Session):
-    hospital = hospital_crud.get_patient(mocked_session, 1)
-    assert hospital.first_name == "Kevin"
+def test_mocked_session_get_hosptital_by_id(mocked_session: hospital_crud.Session):
+    hospital = hospital_crud.get_hospitals(mocked_session, 1)
+    assert Hospital.name == "New York"
