cdd07d65e52d952afed4d3ed15155e7c1720c1f7
oluwatosin amodu <tobunmi2002@yahoo.com>
2024-05-01 14:37:36 -0500

    changes made

--- lib/patient_crud.py
+++ lib/patient_crud.py
@@ -2,6 +2,9 @@
 from fastapi import HTTPException
 from sqlalchemy import asc, desc
 from sqlalchemy.orm import Session
+from sqlalchemy.sql import func
+
+from lib.response_models import PatientUpdate
 
 from . import db_models
 
@@ -43,3 +46,63 @@ def get_patients(
             query_order_by = desc(sort_by)
         return db.query(db_models.Patient).order_by(query_order_by).offset(skip).limit(limit).all()
     return db.query(db_models.Patient).offset(skip).limit(limit).all()
+
+
+def create_patient(db: Session, patient):
+    # Find the physician with the least number of patients
+    subquery = (
+        db.query(db_models.Physician.id, func.count(db_models.Patient.id).label("patient_count"))
+        .outerjoin(db_models.Patient, db_models.Physician.id == db_models.Patient.physician_id)
+        .group_by(db_models.Physician.id)
+        .subquery()
+    )
+
+    least_busy_physician = db.query(subquery.c.id).order_by(subquery.c.patient_count.asc()).first()
+
+    if not least_busy_physician:
+        raise HTTPException(status_code=404, detail="No physician available")
+
+    # Create the patient with the physician_id of the least busy physician
+    patient_data = patient.dict()
+    patient_data["physician_id"] = least_busy_physician.id
+    db_patient = db_models.Patient(**patient_data)
+    db.add(db_patient)
+    db.commit()
+    db.refresh(db_patient)
+    return db_patient
+
+
+# Delete - Remove a patient
+def delete_patient(db: Session, patient_id: int):
+    # First, check if the patient exists
+    db_patient = db.query(db_models.Patient).filter(db_models.Patient.id == patient_id).first()
+    if not db_patient:
+        raise HTTPException(status_code=404, detail="Patient not found")
+
+    # Explicitly delete dependencies
+    # Delete all related appointments
+    db.query(db_models.Appointment).filter(db_models.Appointment.patient_id == patient_id).delete()
+
+    # Delete all related prescriptions
+    db.query(db_models.Prescription).filter(
+        db_models.Prescription.patient_id == patient_id
+    ).delete()
+
+    # Delete all related insurance records
+    db.query(db_models.Insurance).filter(db_models.Insurance.patient_id == patient_id).delete()
+
+    # Finally, delete the patient
+    db.delete(db_patient)
+    db.commit()
+    return {"ok": True}
+
+
+# Update - Update a patient's details
+def update_patient(db: Session, patient_id: int, patient: PatientUpdate):
+    db_patient = db.query(db_models.Patient).filter(db_models.Patient.id == patient_id).first()
+    if not db_patient:
+        raise HTTPException(status_code=404, detail="Patient not found")
+    for var, value in patient.dict(exclude_unset=True).items():
+        setattr(db_patient, var, value)
+    db.commit()
+    return db_patient
